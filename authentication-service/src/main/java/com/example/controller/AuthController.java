25a32f30f99574692640a0fb64fa3f4525ae345d
SyThorng <sunsythorng9999@gmail.com>
2023-10-20 08:31:01 +0700

    already change code to link verify6

--- authentication-service/src/main/java/com/example/controller/AuthController.java
+++ authentication-service/src/main/java/com/example/controller/AuthController.java
@@ -4,6 +4,7 @@ package com.example.controller;
 import com.example.model.request.LoginRequest;
 import com.example.model.request.UserRequest;
 import com.example.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
@@ -19,26 +20,31 @@ public class AuthController {
     }
 
     @PostMapping("/register")
+    @Operation(summary = "register ")
     public ResponseEntity<?> createUser(@RequestBody UserRequest userRequest) {
         return ResponseEntity.ok().body(userService.create(userRequest));
     }
 
     @PostMapping("/login")
+    @Operation(summary = "login ")
     public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
         return ResponseEntity.ok().body(userService.login(loginRequest));
     }
 
     @GetMapping("/verify-email")
+    @Operation(summary = "verify email use in email click link ")
     public RedirectView verifyEmail(@RequestParam String email, @RequestParam String type) {
         return userService.verifyEmail(email, type);
     }
 
-    @PostMapping("/resend-email")
+    @PostMapping("/email")
+    @Operation(summary = "generate verify email again")
     public ResponseEntity<?> generateCode(@RequestParam String email) {
         return ResponseEntity.ok().body(userService.generateLinkVerifyEmail(email, "false", 1, "false"));
     }
 
-    @PostMapping("/generate-email-forget-password")
+    @PostMapping("/forget-password/email")
+    @Operation(summary = "generate verify email for reset password")
     public ResponseEntity<?> generateCodeForget(@RequestParam String email) {
         return ResponseEntity.ok().body(userService.generateEmailForgetPassword(email));
     }
--- authentication-service/src/main/java/com/example/controller/UserController.java
+++ authentication-service/src/main/java/com/example/controller/UserController.java
@@ -4,6 +4,7 @@ import com.example.exception.BadRequestException;
 import com.example.exception.ForbiddenException;
 import com.example.model.request.ProfileRequest;
 import com.example.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.keycloak.KeycloakSecurityContext;
 import org.keycloak.RSATokenVerifier;
@@ -37,27 +38,31 @@ public class UserController {
 
 
     @GetMapping("username")
+    @Operation(summary = "get user by username")
     public ResponseEntity<?> getByUsername(@RequestParam String username) {
         return ResponseEntity.ok().body(userService.getByUserName(username));
     }
 
     @GetMapping("email")
+    @Operation(summary = "get user by email")
     public ResponseEntity<?> getByEmail(@RequestParam String email) {
         return ResponseEntity.ok().body(userService.getByEmail(email));
     }
 
-    @GetMapping
+//    @GetMapping
     public ResponseEntity<?> getAllUser() {
         return ResponseEntity.ok().body(userService.getAllUsers());
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "get user by id (UUID) ")
     public ResponseEntity<?> getById(@PathVariable UUID id) {
         return ResponseEntity.ok().body(userService.getById(id));
     }
 
     @PutMapping
     @SecurityRequirement(name = "auth")
+    @Operation(summary = "update information user current user (token) ")
     public ResponseEntity<?> updateById(@RequestBody ProfileRequest userRequest, Principal principal) {
         return ResponseEntity.ok().body(userService.updateById(userRequest, principal));
 
