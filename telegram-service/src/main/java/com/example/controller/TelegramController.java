8ce1aaa5c6ae0237e9a90065205299afb74723a7
SyThorng <sunsythorng9999@gmail.com>
2023-10-24 13:57:57 +0700

    already telegram-serivcev1

--- telegram-service/src/main/java/com/example/controller/TelegramController.java
+++ telegram-service/src/main/java/com/example/controller/TelegramController.java
@@ -2,6 +2,7 @@ package com.example.controller;
 
 
 import com.example.service.TelegramBotService;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +13,7 @@ import java.security.Principal;
 @RestController
 @RequestMapping("api/v1/telegram")
 @CrossOrigin
+@SecurityRequirement(name = "auth")
 public class TelegramController {
     private final TelegramBotService telegramBotService;
 
